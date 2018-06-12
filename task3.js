e197493dbc70de96a5599538f3c96e101f670086
SergeySergeevichE <serenya04@gmail.com>
2018-06-12 09:09:06 +0300

    изменения в коде

--- task3.js
+++ task3.js
@@ -1,8 +1,5 @@
-function declineOfNumber (number, str1, str2, str3) {
-	if ((number % 100 > 4) && (number % 100 < 20)) return str3;
-	if (number % 10 == 1) return str1;
-	if ((number % 10 > 1) && (number % 10 < 5)) return str2;
-	return str3;
+function declOfNum(number, str) {
+  return str[(number % 10 === 1 && number % 100 !== 11) ? 0 : number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20) ? 1 : 2]
 }
 
 function addTimes(firstTime, secondTime) {
@@ -10,17 +7,10 @@ function addTimes(firstTime, secondTime) {
 	var hours = Math.floor(sumTime / 3600);
 	var minutes = Math.floor((sumTime - hours*3600) / 60);
 	var seconds = sumTime % 60;
-	var time = {
-		hours: hours,
-		minutes: minutes,
-		seconds: seconds,
-		textHours: declineOfNumber(hours,' час',' часа',' часов'),
-		textMinutes: declineOfNumber(minutes, ' минута',' минуты',' минут'),
-		textSeconds: declineOfNumber(seconds ,' секунда',' секунды',' секунд'),
-	};
-	if ((time.hours === 0) && (time.minutes === 0)) return '' + time.seconds + time.textSeconds;
-	if ((time.hours === 0) && (time.minutes != 0)) return  time.minutes + time.textMinutes +' ' + time.seconds + time.textSeconds;
-	return time.hours + time.textHours + ' ' + time.minutes + time.textMinutes +' ' + time.seconds + time.textSeconds;
+	textHours = hours + declOfNum(hours, [' час',' часа',' часов']);
+	textMinutes = minutes + declOfNum(minutes, [' минута',' минуты',' минут']);
+	textSeconds = seconds + declOfNum(seconds ,[' секунда',' секунды',' секунд']);
+	return (hours === 0 && minutes === 0) ? textSeconds : (hours === 0 && minutes !== 0) ? (textMinutes + ' ' + textSeconds) : (textHours + ' ' + textMinutes + ' ' + textSeconds);
 }
 
 var a = process.argv[2];
